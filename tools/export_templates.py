b7845aae62e8fef90f928cd3b831b135f3d723c4
dev-yaroslav-b <dev.yaroslav.b@gmail.com>
2020-03-19 15:44:21 +0200

    Minor change

--- tools/export_templates.py
+++ tools/export_templates.py
@@ -1,74 +1,75 @@
 VERSION = 1.1
 EXPORT_URL = 'https://standards.oftrust.net/v1/'
+PREFIX = 'pot'
 
 
-def BASE_IDENTITY_TEMPLATE(pot_export_url: str) -> dict:
+def BASE_IDENTITY_TEMPLATE(export_onto_url: str) -> dict:
     return {
         '@version': VERSION,
-        '@vocab': f"{pot_export_url}vocabularies/.jsonld#",
+        '@vocab': f'{export_onto_url}vocabularies/.jsonld#',
         '@classDefinition': '',
-        "pot": {
-            "@id": f"{pot_export_url}Vocabulary/",
-            "@prefix": True
+        f'{PREFIX}': {
+            '@id': f'{export_onto_url}Vocabulary/',
+            '@prefix': True
         },
-        "data": "pot:data",
-        "metadata": "pot:metadata",
+        'data': f'{PREFIX}:data',
+        'metadata': f'{PREFIX}:metadata',
     }
 
 
-def DEFENITION_TEMPLATE(pot_export_url: str) -> dict:
+def DEFENITION_TEMPLATE(export_onto_url: str) -> dict:
     return {
-        "@context": {
-            "@version": VERSION,
-            "xsd": {
-                "@id": "http://www.w3.org/2001/XMLSchema#",
-                "@prefix": True
+        '@context': {
+            '@version': VERSION,
+            'xsd': {
+                '@id': 'http://www.w3.org/2001/XMLSchema#',
+                '@prefix': True
             },
-            "pot": {
-                "@id": f"{pot_export_url}Vocabulary/",
-                "@prefix": True
+            f'{PREFIX}': {
+                '@id': f'{export_onto_url}Vocabulary/',
+                '@prefix': True
             },
-            "description": {},
-            "label": {
+            'description': {},
+            'label': {
                 '@id': 'rdfs:label',
-                "@container": ['@language', '@set']
+                '@container': ['@language', '@set']
             },
-            "comment": {
+            'comment': {
                 '@id': 'rdfs:comment',
-                "@container": ['@language', '@set']
+                '@container': ['@language', '@set']
             }
         },
     }
 
 
-def VOCABULARY_TEMPLATE(pot_export_url:str) -> dict:
+def VOCABULARY_TEMPLATE(export_onto_url: str) -> dict:
     return {
-        "@context": {
+        '@context': {
             '@version': VERSION,
-            "pot": {
-                "@id": f"{pot_export_url}Vocabulary/",
-                "@prefix": True
+            f'{PREFIX}': {
+                '@id': f'{export_onto_url}Vocabulary/',
+                '@prefix': True
             },
-            "rdf": {
-                "@id": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                "@prefix": True
+            'rdf': {
+                '@id': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+                '@prefix': True
             },
-            "rdfs": {
-                "@id": "http://www.w3.org/2000/01/rdf-schema#",
-                "@prefix": True
+            'rdfs': {
+                '@id': 'http://www.w3.org/2000/01/rdf-schema#',
+                '@prefix': True
             },
-            "owl": {
-                "@id": "http://www.w3.org/2002/07/owl#",
-                "@prefix": True
+            'owl': {
+                '@id': 'http://www.w3.org/2002/07/owl#',
+                '@prefix': True
             },
-            "vs": {
-                "@id": "http://www.w3.org/2003/06/sw-vocab-status/ns#",
+            'vs': {
+                '@id': 'http://www.w3.org/2003/06/sw-vocab-status/ns#',
             },
-            "xsd": {
-                "@id": "http://www.w3.org/2001/XMLSchema#",
-                "@prefix": True
+            'xsd': {
+                '@id': 'http://www.w3.org/2001/XMLSchema#',
+                '@prefix': True
             },
-            "label": "",
-            "comment": ""
+            'label': '',
+            'comment': ''
         }
     }
