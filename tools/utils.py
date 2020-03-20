79823e657767e62affc031cabc9224c712976a02
dev-yaroslav-b <dev.yaroslav.b@gmail.com>
2020-03-20 20:15:01 +0200

    Add docstring. Minor changes

--- tools/utils.py
+++ tools/utils.py
@@ -1,90 +1,94 @@
 from copy import deepcopy
 from collections import OrderedDict
+from typing import Any, Dict
 
-from owlready2 import Thing, DataPropertyClass, ObjectPropertyClass, ThingClass, AnnotationPropertyClass, default_world, Ontology
+from owlready2 import Thing, default_world
 
-from class_helpers import readonly, required, subPropertyOf, range, restriction, subClassOf, domain, nest
-from builders import build_comments, build_labels, build_nested_comments, build_nested_labels, build_ranges
+from class_helpers import range as owl_range
+from class_helpers import readonly, required, subPropertyOf, restriction, subClassOf, domain, nest
+from builders import build_comments, build_labels, build_nested_comments, build_nested_labels, build_ranges, build_restrictions
+
 
 PREFIX = 'pot'
+A = set()
 
 
-def prop_get_full_id(prop: DataPropertyClass) -> str:
-    """
-    Function to get DataPropertyClass id.
+def prop_get_full_id(owl_property: Any) -> str:
+    """Return property id.
 
-        Parameters:
-            prop (DataPropertyClass): 
-
+        Args:
+            owl_property (Any): 
+                Property of entity.
+                Can be: ObjectPropertyClass, AnnotationPropertyClass, DataPropertyClass.
         Returns:
-            '' (str): Combined property id and property name
+            (str): Combined property id and property name.
     """
     property_id = ''
-    subproperties = list(subPropertyOf._get_indirect_values_for_class(prop))
+    subproperties = list(
+        subPropertyOf._get_indirect_values_for_class(owl_property))
     if subproperties and subproperties[0].name != 'topDataProperty':
         property_id = f'{prop_get_full_id(subproperties[0])}/'
-    return f'{property_id}{prop.name}'
+    return f'{property_id}{owl_property.name}'
 
 
-def class_get_full_id(prop: ThingClass) -> str:
-    """
-    Function to get ThingClass id.
+def class_get_full_id(entity: Any) -> str:
+    """Return ThingClass id.
 
-        Parameters:
-            prop (ThingClass): 
+        Args:
+            entity (Any): ThingClass entity.
 
         Returns:
-            '' (str): Combined class id and property name
+            (str): Combined class id and entity name.
     """
     class_id = ''
-    subclass = list(subClassOf._get_indirect_values_for_class(prop))
+    subclass = list(subClassOf._get_indirect_values_for_class(entity))
     if subclass and subclass[0] != Thing:
         class_id = f'{class_get_full_id(subclass[0])}/'
-    return f'{class_id}{prop.name}'
+    return f'{class_id}{entity.name}'
 
 
-def owl_property_to_python_base(rdf_attribute: DataPropertyClass) -> dict:
-    """
-    Function to transform property to python base. 
-    Generate: labels, nested labels, comments, nested comments
+def owl_property_to_python_base(owl_property: Any) -> Dict[str, Any]:
+    """Return dict of generated parameters using property.
 
-        Parameters:
-            rdf_attribute (DataPropertyClass): 
+        Args:
+            owl_property (Any): 
+                Property of entity.
+                Can be: ObjectPropertyClass, AnnotationPropertyClass, DataPropertyClass.
 
         Returns:
-            result (dict): Dict of property parameters
+            result (dict of str: Any): Dict of generated parameters.
     """
-    _type = default_world._unabbreviate(rdf_attribute._owl_type).replace(
+    _type = default_world._unabbreviate(owl_property._owl_type).replace(
         'http://www.w3.org/2002/07/owl#', 'owl:')
 
     result = {
-        '@id': f'{PREFIX}:{prop_get_full_id(rdf_attribute)}',
+        '@id': f'{PREFIX}:{prop_get_full_id(owl_property)}',
         '@type': _type,
     }
 
     subproperties = list(
-        subPropertyOf._get_indirect_values_for_class(rdf_attribute))
+        subPropertyOf._get_indirect_values_for_class(owl_property))
     if subproperties:
         result['subPropertyOf'] = f'{PREFIX}:{prop_get_full_id(subproperties[0])}'
 
     # TODO
-    labels = build_labels(rdf_attribute)
+    labels = build_labels(owl_property)
     if labels.items():
         result["rdfs:label"] = labels
 
     # TODO
-    comments = build_comments(rdf_attribute)
+    comments = build_comments(owl_property)
     if comments.items():
         result["rdfs:comment"] = comments
 
     # TODO
-    nested_comments = build_nested_comments(rdf_attribute)
-    if len(nested_comments):
+    nested_comments = build_nested_comments(owl_property)
+    if nested_comments:
         result["comment"] = nested_comments
 
     # TODO
-    nested_labes = build_nested_labels(rdf_attribute)
-    if len(nested_labes):
+    nested_labes = build_nested_labels(owl_property)
+    if nested_labes:
         result["label"] = nested_labes
 
     return result
@@ -93,49 +97,51 @@ def owl_property_to_python_base(rdf_attribute: DataPropertyClass) -> dict:
 # Create Definition
 
 
-def owl_property_to_python_for_definition(rdf_attribute: DataPropertyClass) -> dict:
-    """
-    Function to create definitions from rdf classes.
+def owl_property_to_python_for_definition(owl_property: Any) -> Dict[str, Any]:
+    """Return dict of generated properties required for definition.
 
-        Parameters:
-            rdf_attribute (DataPropertyClass): 
+        Args:
+            owl_property (Any): 
+                Property of entity.
+                Can be: DataPropertyClass, ObjectPropertyClass
 
         Returns:
             result (dict): Dict of property parameters
     """
-    result = owl_property_to_python_base(rdf_attribute)
+    result = owl_property_to_python_base(owl_property)
 
-    is_required = required._get_indirect_values_for_class(rdf_attribute)
+    is_required = required._get_indirect_values_for_class(owl_property)
     if is_required:
         result[f'{PREFIX}:required'] = is_required[0]
 
-    is_readonly = readonly._get_indirect_values_for_class(rdf_attribute)
+    is_readonly = readonly._get_indirect_values_for_class(owl_property)
     if is_readonly:
         result[f'{PREFIX}:readonly'] = is_readonly[0]
 
-    if list(range._get_indirect_values_for_class(rdf_attribute)):
-        result[f'{PREFIX}:valueType'] = build_ranges(rdf_attribute, range)
+    if list(owl_range._get_indirect_values_for_class(owl_property)):
+        result[f'{PREFIX}:valueType'] = build_ranges(owl_property)
 
-    if list(restriction._get_indirect_values_for_class(rdf_attribute)):
-        result['xsd:restriction'] = build_ranges(rdf_attribute, restriction)
+    if list(restriction._get_indirect_values_for_class(owl_property)):
+        result['xsd:restriction'] = build_restrictions(owl_property)
 
     return result
 
 
-def create_definition_from_rdf_class(rdf_class, entity_file, onto, export_onto_url: str, template: dict) -> dict:
-    """
-    Function to create definitions from rdf classes.
+def create_definition_from_rdf_class(rdf_class, entity_file: Dict[str, str], onto, export_onto_url: str, template: Dict[str, str]) -> Dict[str, Any]:
+    """Return dict of generated properties to create definitions from rdf classes.
 
-        Parameters:
-            rdf_class :
-            entity_file :
-            onto: An ontology loaded with owlready2.
+        Args:
+            rdf_class (models.RDFClass): RDFClass model object
+            entity_file (dict of str: Any): 
+                Dictionary with directory, filename and id of entity
+            onto (namespace.Ontology): An ontology loaded with owlready2.
             export_onto_url (str): Link to base ontologies.
-            template (dict): Template for definitions.
+            template (dict of str: str): Template for definitions.
 
         Returns:
-            vocabulary_dict (dict): 
+            vocabulary_dict (dict of str: Any): Dictionary of required parameters
     """
+    
     vocabulary = f"{export_onto_url}Vocabulary/{entity_file.get('id')}"
 
     vocabulary_dict = deepcopy(template)
@@ -160,11 +166,11 @@ def create_definition_from_rdf_class(rdf_class, entity_file, onto, export_onto_u
 
     total_attributes = set()
     for a in onto.data_properties():
-        if rdf_class.rdf_entity.ancestors().intersection(a.domain):
+        if rdf_class.entity.ancestors().intersection(a.domain):
             total_attributes.add(a)
 
     for a in onto.object_properties():
-        if rdf_class.rdf_entity.ancestors().intersection(a.domain):
+        if rdf_class.entity.ancestors().intersection(a.domain):
             total_attributes.add(a)
 
     for rdf_attribute in total_attributes:
@@ -176,34 +182,45 @@ def create_definition_from_rdf_class(rdf_class, entity_file, onto, export_onto_u
 
     supported_class[f'{PREFIX}:supportedAttribute'] = supported_attrs
     vocabulary_dict[f'{PREFIX}:supportedClass'] = supported_class
+    
     return vocabulary_dict
 
 # Create Identity
 
 
-def owl_property_context(rdf_attribute: DataPropertyClass) -> dict:
+def owl_property_context(owl_property: Any) -> Dict[str, str]:
+    """Return dict of generated nest parameter.
+
+        Args:
+            owl_property (Any): 
+                Property of entity.
+                Can be: DataPropertyClass, ObjectPropertyClass
+
+        Returns:
+            result (dict of str: str): Dictionary of nest parameters
+    """
     result = {
-        '@id': f'{PREFIX}:{prop_get_full_id(rdf_attribute)}'
+        '@id': f'{PREFIX}:{prop_get_full_id(owl_property)}'
     }
-    nest_list = nest._get_indirect_values_for_class(rdf_attribute)
+    nest_list = nest._get_indirect_values_for_class(owl_property)
     if nest_list:
         result['@nest'] = nest_list[0].name
     return result
 
 
-def create_identity_from_rdf_class(rdf_class, entity_file, onto, export_onto_url: str, template: dict) -> dict:
-    """
-    Function to create identities from rdf classes.
+def create_identity_from_rdf_class(rdf_class, entity_file: Dict[str, Any], onto, export_onto_url: str, template: Dict[str, str]) -> Dict[str, Any]:
+    """Return dict of generated properties to create identity from rdf classes.
 
-        Parameters:
-            rdf_class :
-            entity_file :
-            onto: An ontology loaded with owlready2.
+        Args:
+            rdf_class (models.RDFClass): RDFClass model object.
+            entity_file (dict of str: Any): 
+                Dictionary with directory, filename and id of entity.
+            onto (namespace.Ontology): An ontology loaded with owlready2.
             export_onto_url (str): Link to base ontologies.
-            template (dict): Template for definitions.
+            template (dict of str: str): Template for definitions.
 
         Returns:
-            {} (dict): 
+            (dict of str: Any): Dictionary of required parameters.
     """
     vocabulary = f"{export_onto_url}ClassDefinitions/{entity_file.get('id')}"
     identity_dict = deepcopy(template)
@@ -212,11 +229,11 @@ def create_identity_from_rdf_class(rdf_class, entity_file, onto, export_onto_url
 
     total_attributes = set()
     for a in onto.data_properties():
-        if rdf_class.rdf_entity.ancestors().intersection(a.domain):
+        if rdf_class.entity.ancestors().intersection(a.domain):
             total_attributes.add(a)
 
     for a in onto.object_properties():
-        if rdf_class.rdf_entity.ancestors().intersection(a.domain):
+        if rdf_class.entity.ancestors().intersection(a.domain):
             total_attributes.add(a)
 
     for rdf_attribute in total_attributes:
@@ -229,17 +246,27 @@ def create_identity_from_rdf_class(rdf_class, entity_file, onto, export_onto_url
 # Create Vocabulary
 
 
-def owl_property_to_python_for_vocabulary(rdf_attribute: DataPropertyClass) -> dict:
-    result = owl_property_to_python_base(rdf_attribute)
+def owl_property_to_python_for_vocabulary(owl_property) -> dict:
+    """Return dict of generated properties required for vocabulary.
 
-    if list(range._get_indirect_values_for_class(rdf_attribute)):
-        result[f'{PREFIX}:valueType'] = build_ranges(rdf_attribute, range)
+        Args:
+            owl_property (Any): 
+                Property of entity.
+                Can be: DataPropertyClass, ObjectPropertyClass, AnnotationPropertyClass.
 
-    if list(restriction._get_indirect_values_for_class(rdf_attribute)):
-        result['xsd:restriction'] = build_ranges(rdf_attribute, restriction)
+        Returns:
+            result (dict): Dict of property parameters.
+    """
+    result = owl_property_to_python_base(owl_property)
+
+    if list(owl_range._get_indirect_values_for_class(owl_property)):
+        result[f'{PREFIX}:valueType'] = build_ranges(owl_property)
+
+    if list(restriction._get_indirect_values_for_class(owl_property)):
+        result['xsd:restriction'] = build_restrictions(owl_property)
 
     domains = []
-    for d in domain._get_indirect_values_for_class(rdf_attribute):
+    for d in domain._get_indirect_values_for_class(owl_property):
         domains.append(f'{PREFIX}:{d.name}')
     if domains:
         result['domain'] = domains
@@ -247,22 +274,34 @@ def owl_property_to_python_for_vocabulary(rdf_attribute: DataPropertyClass) -> d
     return result
 
 
-def create_vocabulary_from_rdf_class(rdf_class, entity_file: dict, onto: Ontology, template: dict) -> dict:
+def create_vocabulary_from_rdf_class(rdf_class, entity_file: Dict[str, Any], onto, template: Dict[str, str]) -> Dict[str, Any]:
+    """Return dict of generated properties to create vocabulary from rdf classes.
+
+        Args:
+            rdf_class (models.RDFClass): RDFClass model object.
+            entity_file (dict of str: Any): 
+                Dictionary with directory, filename and id of entity.
+            onto (namespace.Ontology): An ontology loaded with owlready2.
+            template (dict of str: str): Template for definitions.
+
+        Returns:
+            vocabulary_dict (dict of str: Any): Dictionary of required parameters.
+    """
     vocabulary_dict = deepcopy(template)
     total_attributes = set()
     for a in onto.data_properties():
-        if rdf_class.rdf_entity.ancestors().intersection(a.domain):
+        if rdf_class.entity.ancestors().intersection(a.domain):
             total_attributes.add(a)
 
     for a in onto.object_properties():
-        if rdf_class.rdf_entity.ancestors().intersection(a.domain):
+        if rdf_class.entity.ancestors().intersection(a.domain):
             total_attributes.add(a)
 
     for rdf_attribute in total_attributes:
         vocabulary_dict[rdf_attribute.name] = owl_property_to_python_for_vocabulary(
             rdf_attribute)
 
-    vocabulary_dict[rdf_class.rdf_entity.name] = rdf_class.to_python(
+    vocabulary_dict[rdf_class.entity.name] = rdf_class.to_python(
         entity_file)
     vocabulary_dict['@context']['label'] = {
         '@id': 'rdfs:label',
@@ -273,7 +312,7 @@ def create_vocabulary_from_rdf_class(rdf_class, entity_file: dict, onto: Ontolog
         "@container": ['@language', '@set']
     }
 
-    for dependent in rdf_class.rdf_entity.subclasses():
+    for dependent in rdf_class.entity.subclasses():
         vocabulary_dict[dependent.name] = {
             'rdfs:subClassOf': {
                 '@id': f'{PREFIX}:{class_get_full_id(dependent).replace(f"/{dependent.name}", "")}'
